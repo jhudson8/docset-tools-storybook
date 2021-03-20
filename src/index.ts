7d720a99bf9bf403a813dda49f205087085b6bb0
Joe Hudson <joehud@gmail.com>
2021-03-20 13:24:47 -0400

    graceful fallback

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { Plugin, DocsetEntries, getKnownType } from "docset-tools-types";
-import { existsSync } from "fs-extra";
+import { existsSync, mkdirSync } from "fs-extra";
 import { join } from "path";
+import rimraf from 'rimraf';
 
 const DEFAULT_STORYBOOK_DIR = "storybook-static";
 
@@ -10,35 +11,51 @@ const plugin: Plugin = {
       ? getKnownType(pluginOptions.docsetType)
       : "Component";
     const storybookDir = pluginOptions.storybookDir || DEFAULT_STORYBOOK_DIR;
+    const storybookTmpPath = join(process.cwd(), '._tmp_storybook_public');
+    let storybookMetaRetrieved = false;
+
     try {
+      // make the ._tmp_storybook_public dir
+      if (!existsSync(storybookTmpPath)){
+        mkdirSync(storybookTmpPath);
+      }
+
       await spawn("node", [
         join(
           process.cwd(),
           "node_modules/@storybook/react/dist/server/build.js"
         ),
         "-s",
-        "public",
+        "._tmp_storybook_public",
         "--docs",
       ]);
-      await spawn("node", [
-        join(process.cwd(), "node_modules/@storybook/cli/bin/index.js"),
-        "extract",
-      ]);
+      try {
+        await spawn("node", [
+          join(process.cwd(), "node_modules/@storybook/cli/bin/index.js"),
+          "extract",
+        ]);
+        storybookMetaRetrieved = true;
+      } catch (e) {
+        // we couldn't get story details so we'll have a single storybook entry
+      }
     } catch (e) {
       throw new Error(
         "Unable to get storybook output our metadata.\n\t" +
           "Make sure you have storybook >= 6 AND @storybook/react >= 6\n\t\t" +
           "Try running commands below directly and see if there are any errors and make sure you are using storybook >= 6\n\t\t" +
-          "node node_modules/@storybook/react/dist/server/build.js -s public\n\t\t" +
+          "node node_modules/@storybook/react/dist/server/build.js -s ._tmp_storybook_public\n\t\t" +
           "node node_modules/@storybook/cli/bin/index.js extract\n\t" +
           "if extract doesn't work and you are behind a firewall, the PUPPETEER_ env variables will not work unless you modify `resolveExecutablePath` in puppeteer-core/lib/Launcher.js\n\t" +
           "change if block to `if (!launcher._isPuppeteerCore || true)` and set PUPPETEER_EXECUTABLE_PATH environment variable to Chrome executable location"
       );
+    } finally {
+      // remove the tmp path we created
+      rimraf.sync(storybookTmpPath);
     }
 
     const storybookDirPath = join(process.cwd(), storybookDir);
     const storybookMetaPath = join(storybookDirPath, "stories.json");
-    const exists = existsSync(storybookMetaPath);
+    const exists = !storybookMetaRetrieved || existsSync(storybookMetaPath);
     if (!exists) {
       throw new Error("Could not create storybook artifacts");
     }
@@ -46,20 +63,24 @@ const plugin: Plugin = {
       [docsetType]: {},
     };
 
-    const storybookMeta = require(storybookMetaPath) as any;
-    const stories = Object.values(storybookMeta.stories);
-    for (let i = 0; i < stories.length; i++) {
-      const storyMeta = stories[i] as any;
-      if (!storyMeta.kind.match(/Example\/Introduction/i)) {
-        const url = `storybook/index.html?path=/story/${storyMeta.id}`;
-        const path = `${storyMeta.kind.replace(/^examples?\//i, "")}/${
-          storyMeta.name
-        }`;
-        (rtn as any)[docsetType][path] = url;
+    if (storybookMetaRetrieved) {
+      const storybookMeta = require(storybookMetaPath) as any;
+      const stories = Object.values(storybookMeta.stories);
+      for (let i = 0; i < stories.length; i++) {
+        const storyMeta = stories[i] as any;
+        if (!storyMeta.kind.match(/Example\/Introduction/i)) {
+          const url = `storybook/index.html?path=/story/${storyMeta.id}`;
+          const path = `${storyMeta.kind.replace(/^examples?\//i, "")}/${
+            storyMeta.name
+          }`;
+          (rtn as any)[docsetType][path] = url;
+        }
       }
+    } else {
+      (rtn as any)[docsetType].Storybooks = 'storybook/index.html';
     }
 
-    const storybookSettings = `
+    const storybookSettings = storybookMetaPath ? `
     <script>
       sessionStorage.setItem('@storybook/ui/store', JSON.stringify({
         layout: {
@@ -71,7 +92,7 @@ const plugin: Plugin = {
         }
       }))
     </script>
-    `;
+    ` : ``;
 
     await include({
       path: storybookDirPath,
